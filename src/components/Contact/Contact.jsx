cc8c0551c54abf6a504a17abe1b5a96ebd13bbd3
Sang Hyun Lee <you@example.com>
2021-09-14 01:32:01 -0400

    contact update

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,14 +3,41 @@ import './Contact.css'
 import {Email, LinkedIn} from '@material-ui/icons'
 
 export default function Contact() {
+
   return (
     <div className="contactDiv">
       <section id="contact">
         <br />
         <hr />
+        <br />
+    
         <p>
           <span id="contactMsg">PLEASE FEEL FREE TO REACH ME!</span><br />
           <br />
+          <div className="contact-box">
+            <form action="https://formspree.io/f/mayavnow" method="POST" id="my-form">
+              <div className="form-group">
+                <label for="yourName">NAME</label>
+                <input type="text" id="yourName" name="yourName"></input>
+              </div>
+              <div className="form-group">
+                <label for="yourEmail">EMAIL</label>
+                <input type="email" id="yourEmail" name="yourEmail"></input>
+              </div>
+              <div className="form-group">
+                <label for="subject">SUBJECT</label>
+                <input type="text" id="subject" name="subject"></input>
+              </div>
+              <div className="form-group">
+                <label for="message">MESSAGE</label>
+                <textarea type="text" id="message" name="message" cols="30" rows="10"></textarea>
+              </div>
+              <button type="submit">Send Message</button>
+            </form>
+          </div>
+          <div id="status"></div>
+
+
           <a id="email" href={`mailto:ryanslee91@gmail.com`}>
             <Email/>
           </a>
