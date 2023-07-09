57786361c980e5398bb102fd493eca0fa608914f
Ryan Lee <ryanslee91@gmail.com>
2023-07-09 15:41:05 -0400

    update

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -8,33 +8,11 @@ export default function AboutMe() {
         <p className="aboutPara">
           <h4>ABOUT ME</h4>
           I am a Malvern-based full-stack software engineer currently working for the Vanguard Group. <br />
-          After completing my degree in Politics at New York University, <br />
+          After completing my bachelor's degree at New York University, <br />
           I recalled my first ever experience of web management as a child back in 2002.<br />
           About two decades later, I have become lucky to find my passion in web development again.<br />
-          I dream of becoming a creative web developer who enjoys challenging with passion.<br />
+          I dream of becoming a creative developer who enjoys challenging with passion.<br />
     <br />
-          <div className="skillsDiv">
-           <b> Hard Skills</b> <br />
-
-            HTML /
-            CSS /
-            JAVASCRIPT /
-            TYPESCRIPT<br />
-  
-            ANGULAR /
-            REACT /
-            NODE.JS<br />
-    
-            AWS CLOUD PRACTITIONER /
-            AWS WEB SERVICES /
-            DYNAMODB<br />
-
-            NETLIFY /
-            BAMBOO /
-            POSTMAN /
-            SPLUNK /
-            CYPRESS<br />
-        </div>
       </p>
       </section>
     </div>
